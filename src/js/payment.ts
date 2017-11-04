bdad232af732077af3fd98148e26e71f70a56bea
[muweigg] <[muweigg@gmail.com]>
2017-11-04 17:49:08 +0800

    update

--- src/js/payment.ts
+++ src/js/payment.ts
@@ -1,6 +1,7 @@
 $(() => {
     let loginIGB = $('.login-igb button');
     let confirmPay = $('.confirm-pay button');
+    let searchBar = $('.search-bar i');
 
     loginIGB.click(function () {
         $('.login-igb').hide();
@@ -9,5 +10,9 @@ $(() => {
 
     confirmPay.click(function () {
         $('.confirm-pay').hide();
+    });
+
+    searchBar.click(function () {
+        $('.search-bar').toggleClass('open');
     });
 });
