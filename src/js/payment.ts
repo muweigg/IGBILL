5fb741cb9437b79016b768ecc6d553d8c387c0d2
[muweigg] <[muweigg@gmail.com]>
2017-11-05 11:34:27 +0800

    update

--- src/js/payment.ts
+++ src/js/payment.ts
@@ -2,6 +2,7 @@ $(() => {
     let loginIGB = $('.login-igb button');
     let confirmPay = $('.confirm-pay button');
     let searchBar = $('.search-bar i');
+    let sort = $('.skins .all-and-sort .sort');
 
     loginIGB.click(function () {
         $('.login-igb').hide();
@@ -14,5 +15,9 @@ $(() => {
 
     searchBar.click(function () {
         $('.search-bar').toggleClass('open');
+    });
+
+    sort.click(function () {
+        $(this).toggleClass('reverse');
     });
 });
