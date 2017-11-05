ff5bb58b968d1fa8a20822ef24336f04326777ad
[muweigg] <[muweigg@gmail.com]>
2017-11-05 11:59:47 +0800

    update

--- src/js/payment.ts
+++ src/js/payment.ts
@@ -3,6 +3,7 @@ $(() => {
     let confirmPay = $('.confirm-pay button');
     let searchBar = $('.search-bar i');
     let sort = $('.skins .all-and-sort .sort');
+    let pay = $('.balance button');
 
     loginIGB.click(function () {
         $('.login-igb').hide();
@@ -11,10 +12,23 @@ $(() => {
 
     confirmPay.click(function () {
         $('.confirm-pay').hide();
+        $('.chose-skins').show();
     });
 
     searchBar.click(function () {
         $('.search-bar').toggleClass('open');
+    });
+
+    pay.click(function () {
+        $(this).addClass('disabled').find('span').text('等待中...');
+        setTimeout(() => {
+            $('.waiting').show();
+
+            setTimeout(() => {
+                $('.container-wrap').eq(0).hide();
+                $('.pay-results').show();
+            }, 1000);
+        }, 1000);
     });
 
     sort.click(function () {
