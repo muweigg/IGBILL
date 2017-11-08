d9d8095c7452d4ae863bead55a599a806b058ccf
[muweigg] <[muweigg@gmail.com]>
2017-11-08 16:41:36 +0800

    update

---
+++ src/js/skin-recharge.ts
@@ -0,0 +1,18 @@
+$(() => {
+    let searchBar = $('.search-bar i');
+    let sort = $('.skins .all-and-sort .sort');
+    let pay = $('.chose-skins header button');
+
+    searchBar.click(function () {
+        $('.search-bar').toggleClass('open');
+    });
+
+    sort.click(function () {
+        $(this).toggleClass('reverse');
+    });
+    
+    pay.click(function () {
+        $(this).addClass('disabled').find('span').text('等待中...');
+        setTimeout(() => $('.waiting').show(), 1000);
+    });
+});
