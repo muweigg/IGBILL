bfaeee6782d0b221a2138dfd17775ca74c6f80fa
[muweigg] <[muweigg@gmail.com]>
2017-11-14 13:39:59 +0800

    update

--- src/js/common.ts
+++ src/js/common.ts
@@ -16,25 +16,44 @@ $(() => {
             else header.classList.remove('fixed');
         });
 
-    // 对话框
     window.dialog = {
-        el: document.querySelector('.load-steam-error'),
-        cb: null,
+        el: document.querySelector('.common-dialog'),
+        titleEl: null,
+        contentEl: null,
+        okCB: null,
+        cancelCB: null,
         init: function () {
-            Rx.Observable.fromEvent(this.el.querySelector('.dialog-overlay'), 'click').subscribe(() => this.close());
-            Rx.Observable.fromEvent(this.el.querySelector('.dialog-close'), 'click').subscribe(() => this.close());
-            Rx.Observable.fromEvent(this.el.querySelector('.buttons button'), 'click').subscribe(() => this.close());
+            this.titleEl = $(this.el.querySelector('.dialog-content p:nth-of-type(1)'));
+            this.contentEl = $(this.el.querySelector('.dialog-content p:nth-last-of-type(1)'));
+            Rx.Observable.fromEvent(this.el.querySelector('.dialog-overlay'), 'click').subscribe(() => this.cancel());
+            Rx.Observable.fromEvent(this.el.querySelector('.dialog-close'), 'click').subscribe(() => this.cancel());
+            Rx.Observable.fromEvent(this.el.querySelector('.buttons button:nth-of-type(1)'), 'click').subscribe(() => this.ok());
+            Rx.Observable.fromEvent(this.el.querySelector('.buttons button:nth-last-of-type(1)'), 'click').subscribe(() => this.cancel());
         },
-        open: function (cb) {
+        open: function (options = { msg: '', title: '信息提示', okCB: null, cancelCB: null }) {
             this.el.classList.add('active');
-            this.cb = cb;
+            this.titleEl.text(options.title);
+            this.contentEl.text(options.msg);
+            this.okCB = options.okCB;
+            this.cancelCB = options.cancelCB;
         },
-        close: function () {
+        ok: function () {
+            console.log(typeof this.okCB);
+            if (typeof this.okCB === 'function') this.okCB();
             this.el.classList.remove('active');
-            if (typeof this.cb === 'function') this.cb();
+        },
+        cancel: function () {
+            if (typeof this.cancelCB === 'function') this.cancelCB();
+            this.el.classList.remove('active');
         }
     };
     window.dialog.init();
+    window.dialog.open({
+        msg: '纳尼！！',
+        title: '尼玛',
+        okCB: () => alert('ok...'),
+        cancelCB: () => alert('cancel...')
+    });
 
     // 移动端
     if (window.isMobile) {
--- src/js/skin-recharge.ts
+++ src/js/skin-recharge.ts
@@ -15,8 +15,28 @@ $(() => {
         $(this).addClass('disabled').find('span').text('等待中...');
         setTimeout(() => {
             $('.waiting').show();
-            setTimeout(() => window.dialog.open(() => alert('callback...')), 1000);
+            setTimeout(() => loadErrorDialog.open(() => alert('callback...')), 1000);
         }, 1000);
     });
+    
+    // 对话框
+    let loadErrorDialog = {
+        el: document.querySelector('.load-steam-error'),
+        cb: null,
+        init: function () {
+            Rx.Observable.fromEvent(this.el.querySelector('.dialog-overlay'), 'click').subscribe(() => this.close());
+            Rx.Observable.fromEvent(this.el.querySelector('.dialog-close'), 'click').subscribe(() => this.close());
+            Rx.Observable.fromEvent(this.el.querySelector('.buttons button'), 'click').subscribe(() => this.close());
+        },
+        open: function (cb) {
+            this.el.classList.add('active');
+            this.cb = cb;
+        },
+        close: function () {
+            this.el.classList.remove('active');
+            if (typeof this.cb === 'function') this.cb();
+        }
+    };
+    loadErrorDialog.init();
 
 });
