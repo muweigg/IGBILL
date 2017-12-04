9472e19cf2ea0651b0fff4385c4fefec90241711
[muweigg] <[muweigg@gmail.com]>
2017-12-04 11:31:22 +0800

    update

--- src/js/common/terms-dialog.ts
+++ src/js/common/terms-dialog.ts
@@ -5,16 +5,24 @@ $(() => {
         window.termsDialog = {
             el: termsDialog,
             okCB: null,
+            closeCB: null,
             init: function () {
                 Rx.Observable.fromEvent(this.el.querySelector('.dialog-close'), 'click').subscribe(() => this.close());
-                Rx.Observable.fromEvent(this.el.querySelector('.buttons button:nth-of-type(1)'), 'click').subscribe(() => this.close());
+                Rx.Observable.fromEvent(this.el.querySelector('.buttons button:nth-of-type(1)'), 'click').subscribe(() => this.ok());
             },
-            open: function () {
+            open: function (okCB, closeCB) {
                 this.el.classList.add('active');
+                this.okCB = okCB;
+                this.closeCB = closeCB;
             },
-            close: function () {
+            ok: function () {
+                if (typeof this.okCB === 'function') this.okCB();
                 this.el.classList.remove('active');
             },
+            close: function () {
+                if (typeof this.closeCB === 'function') this.closeCB();
+                this.el.classList.remove('active');
+            }
         };
         window.termsDialog.init();
     }
