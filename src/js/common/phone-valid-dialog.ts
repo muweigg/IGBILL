d5af122bfb8b09af0611b4510acc3cc0f6e719e6
[muweigg] <[muweigg@gmail.com]>
2017-12-04 17:47:41 +0800

    update

--- src/js/common/phone-valid-dialog.ts
+++ src/js/common/phone-valid-dialog.ts
@@ -9,6 +9,7 @@ $(() => {
             init: function () {
                 Rx.Observable.fromEvent(this.el.querySelector('.dialog-close'), 'click').subscribe(() => this.close());
                 Rx.Observable.fromEvent(this.el.querySelector('.buttons button:nth-of-type(1)'), 'click').subscribe(() => this.ok());
+                Rx.Observable.fromEvent(this.el.querySelector('.buttons button:nth-last-of-type(1)'), 'click').subscribe(() => this.close());
             },
             open: function (okCB, closeCB) {
                 this.el.classList.add('active');
